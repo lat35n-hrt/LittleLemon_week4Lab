e4effb2a77f35e5b08c4b08bf121cef946de7254
Michael White <hrt666.cs@gmail.com>
2023-03-05 19:39:15 +0900

    Fixed SystemCheckError in models.py

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -27,7 +27,7 @@ class Cart(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='delivery_crew', null=True)
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(db_index=True, default=False)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(db_index=True)
 
@@ -35,7 +35,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(User, on_delete=models.CASCADE)
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=models.CASCADE)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
