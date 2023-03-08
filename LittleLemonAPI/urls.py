11e4d5d53863b36af5500fd36235a764a5479265
Michael White <hrt666.cs@gmail.com>
2023-03-08 18:07:16 +0900

    style: [endpoint] Removed trailing slash

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/groups/manager/users/', views.ManagerUserList.as_view()),
-    path('api/groups/manager/users/<int:pk>/', views.ManagerUserDetail.as_view()),
-    path('api/groups/delivery-crew/users/', views.DeliveryCrewUserList.as_view()),
-    path('api/groups/delivery-crew/users/<int:pk>/', views.DeliveryCrewUserDetail.as_view()),
+    path('api/groups/manager/users', views.ManagerUserList.as_view()),
+    path('api/groups/manager/users/<int:pk>', views.ManagerUserDetail.as_view()),
+    path('api/groups/delivery-crew/users', views.DeliveryCrewUserList.as_view()),
+    path('api/groups/delivery-crew/users/<int:pk>', views.DeliveryCrewUserDetail.as_view()),
 ]
