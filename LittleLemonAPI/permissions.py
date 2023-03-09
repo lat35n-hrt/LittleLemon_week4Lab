5c11f45f56deb3412e880b255b0638b9a0faa6cd
Michael White <hrt666.cs@gmail.com>
2023-03-10 07:28:41 +0900

    style: alinged IsAuthenticated following BasePermission in a single line

--- LittleLemonAPI/permissions.py
+++ LittleLemonAPI/permissions.py
@@ -1,5 +1,4 @@
-from rest_framework.permissions import BasePermission
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission, IsAuthenticated
 from django.conf import settings
 
 class IsManager(BasePermission):
