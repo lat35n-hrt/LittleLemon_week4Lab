dde1f28461d4a60ab4da9a02ab63d93198fa8db2
Michael White <hrt666.cs@gmail.com>
2023-03-09 11:39:28 +0900

    style:[package]remove APIView, and sort the order

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User, Group
+from django.conf import settings
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .serializers import UserSerializer
-from django.conf import settings
-from django.shortcuts import get_object_or_404
 from .permissions import IsManager
 
 
