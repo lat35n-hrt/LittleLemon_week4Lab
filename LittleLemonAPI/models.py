a5f9a1de9f637441be7e39fcf7fc3ac06f13b635
Michael White <hrt666.cs@gmail.com>
2023-04-05 14:24:27 +0900

    fix: allow admin to view title of Category and MenuItem instead of default numbering

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -6,6 +6,8 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
@@ -13,6 +15,8 @@ class MenuItem(models.Model):
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.title
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
